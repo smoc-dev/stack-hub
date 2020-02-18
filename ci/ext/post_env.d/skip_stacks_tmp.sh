92cc1ed719db5852c60c887e254efb0436ffb473
JAROSLAW M GAWOR <jgawor@us.ibm.com>
2020-02-18 11:25:31 -0500

    Collections 0.6.0 rc.1 (#31)
    * sync with collection 0.6.0-rc.1 execpt incubator/java-spring-boot2
    
    * additional fixes
    
    * skip java-spring-boot2 for now
    
    * make it executable
    
    * exclude twas stack

---
+++ ci/ext/post_env.d/skip_stacks_tmp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# TEMPORARY ONLY
+
+export EXCLUDED_STACKS="incubator/java-spring-boot2 incubator/java-websphere-traditional"
--- ci/ext/pre_env.d/kabanero_pre_env.sh
+++ ci/ext/pre_env.d/kabanero_pre_env.sh
@@ -2,29 +2,46 @@
 set -e
 
 # Setup the environment variable needed to build Kabanero Collections
-if [ -z $BUILD_ALL ]; then
+if [ -z "${BUILD_ALL}" ]; then
     export BUILD_ALL=true
 fi
-if [ -z $REPO_LIST ]; then
+if [ -z "${REPO_LIST}" ]; then
     export REPO_LIST=incubator
 fi
-if [ -z $EXCLUDED_STACKS ]; then
+if [ -z "${EXCLUDED_STACKS}" ]; then
     export EXCLUDED_STACKS=""
 fi
-if [ -z $CODEWIND_INDEX ]; then
+if [ -z "${CODEWIND_INDEX}" ]; then
     export CODEWIND_INDEX=true
 fi
-if [ -z $INDEX_IMAGE ]; then
+if [ -z "${INDEX_IMAGE}" ]; then
     export INDEX_IMAGE=kabanero-index
-fi 
-if [ -z "$DISPLAY_NAME_PREFIX" ]
+fi
+if [ -z "${DISPLAY_NAME_PREFIX}" ]
 then
     export DISPLAY_NAME_PREFIX="Kabanero"
 fi
-if [ -z "$IMAGE_REGISTRY_ORG" ]
+if [ -z "${IMAGE_REGISTRY_ORG}" ]
 then
     export IMAGE_REGISTRY_ORG="kabanero"
 fi
-if [ -z "$LATEST_RELEASE" ]; then
+if [ -z "${LATEST_RELEASE}" ]; then
     export LATEST_RELEASE=true
 fi
+if [ "$TRAVIS" == "true" ]
+then
+    if [ $TRAVIS_TAG ] && [[ $TRAVIS_TAG =~ ^.*-(alpha|beta|rc)\.[0-9]* ]]
+    then
+        if [ -z "${BETA_REGISTRY_USERNAME}" ] || [ -z "${BETA_REGISTRY_PASSWORD}" ] || [ -z "${BETA_REGISTRY_ORG}" ]
+        then
+            echo "ERROR: Non-production release is being created but not all required BETA environment variables have been setup"
+            echo "       Required variables are: BETA_REGISTRY_USERNAME, BETA_REGISTRY_PASSWORD and BETA_REGISTRY_ORG"
+            exit 1
+        else
+            export IMAGE_REGISTRY_USERNAME="${BETA_REGISTRY_USERNAME}"
+            export IMAGE_REGISTRY_PASSWORD="${BETA_REGISTRY_PASSWORD}"
+            export IMAGE_REGISTRY_ORG="${BETA_REGISTRY_ORG}"
+        fi
+    fi
+fi
+
